1b3fa40e1719d1f1dbc6601db6769f68cb208eb3
JakobNeumueller <jakobmartinneumueller@gmail.com>
2022-01-18 09:17:22 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'demo-1_2022_01_18';
+}
